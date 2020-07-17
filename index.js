c87b862048425a31ce93f5e30a0161a7a0cae0e2
ReginaF2012 <skritschy@gmail.com>
2020-07-16 19:27:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, typeOfGift) {
+    let i = 0;
+    let sentence = [];
+    while (i < names.length) {
+      sentence.push(`Thank you, ${names[i]}, for the wonderful ${typeOfGift} gift!`) ;
+      i++; 
+    }
+   
+    return sentence;
+  }
+
+  function countDown(number) {
+    while (number > -1 ) {
+
+        console.log(number)
+        number -= 1;
+      }
+  }
